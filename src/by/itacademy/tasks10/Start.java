70cc74b9d3b8abcb345f799a2772f12b4909b678
insrchofsnrs <e.vavilkin@gmail.com>
2017-07-10 08:10:23 +0300

    Домашка с мапами бля

---
+++ src/by/itacademy/tasks10/Start.java
@@ -0,0 +1,12 @@
+package by.itacademy.tasks10;
+
+import by.itacademy.tasks10.task102.Task102;
+
+/**
+ * Created by Smile on 09.07.17.
+ */
+public class Start {
+    public static void main(String[] args) {
+        Task102.task102(" что то :( :( ;( №( (  что фцв 123 ");
+    }
+}
---
+++ src/by/itacademy/tasks10/task102/Task102.java
@@ -0,0 +1,12 @@
+package by.itacademy.tasks10.task102;
+
+/**
+ * Created by Smile on 09.07.17.
+ */
+public class Task102 {
+    public static void task102(String str){
+        String replace = str.replace(":(", ":)");
+        System.out.println(replace);
+
+    }
+}
